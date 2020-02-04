c308135e57f95c7f025161e7cbad3fc53d300c2d
Ohpe <704560+ohpe@users.noreply.github.com>
2020-02-04 12:07:00 +0100

    Update Edge to 79 (Blink) #2

--- src/desktop/edge.js
+++ src/desktop/edge.js
@@ -1,42 +1,43 @@
 import { detectOS } from '../os';
-import { ENGINE_EDGE, detectEngine } from '../engine';
+import { detectEngine, ENGINE_BLINK, ENGINE_EDGE } from '../engine';
 import { hasFeature } from '../feature';
 
 export function detectEdge() {
-  var browser = 'Microsoft Edge';
-  var browserVersion;
+  var name = 'Microsoft Edge';
   var engine = detectEngine();
   var os = detectOS();
+  var version;
 
   // Allowed Engines:
-  if ([ENGINE_EDGE].indexOf(engine) === -1) {
+  // - EDGE
+  // - BLINK https://blogs.windows.com/msedgedev/2019/11/04/edge-chromium-release-candidate-get-ready/
+  if ([ENGINE_EDGE, ENGINE_BLINK].indexOf(engine) === -1) {
     return;
   }
 
-  if (hasFeature('AuthenticatorAssertionResponse')) {
-    browserVersion = 18;
-  } else if (hasFeature('Client')) {
-    browserVersion = 17;
-  } else if (hasFeature('AbortController')) {
-    browserVersion = 16;
-  } else if (hasFeature('CanvasRenderingContext2D.imageSmoothingEnabled')) {
-    browserVersion = 15;
-  } else if (hasFeature('AudioContext.close')) {
-    browserVersion = 14;
-  } else if (hasFeature('AudioBuffer.copyFromChannel')) {
-    browserVersion = 13;
-  } else if (hasFeature('ANGLE_instanced_arrays.drawArraysInstancedANGLE')) {
-    browserVersion = 12;
+  if ([ENGINE_BLINK].indexOf(engine) !== -1) {
+    if (hasFeature('RTCIceCandidate.address')) {
+      version = 79;
+    }
+  } else if ([ENGINE_EDGE].indexOf(engine) !== -1) {
+    if (hasFeature('AuthenticatorAssertionResponse')) {
+      version = 44; //18
+    } else if (hasFeature('Client')) {
+      version = 42; //17
+    } else if (hasFeature('AbortController')) {
+      version = 41; //16
+    } else if (hasFeature('CanvasRenderingContext2D.imageSmoothingEnabled')) {
+      version = 40; //15
+    } else if (hasFeature('AudioContext.close')) {
+      version = 38; //14
+    } else if (hasFeature('AudioBuffer.copyFromChannel')) {
+      version = 25; //13
+    } else if (hasFeature('ANGLE_instanced_arrays.drawArraysInstancedANGLE')) {
+      version = 20; //12
+    }
   }
 
-  if (browserVersion) {
-    return {
-      browser: browser,
-      browserVersion: browserVersion,
-      engine: engine,
-      engineVersion: undefined,
-      os: os,
-      osVersion: undefined
-    };
+  if (version) {
+    return { name: name, engine: engine, version: version, os: os };
   }
 }
