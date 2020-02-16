f6ad90f32269784af1b83fa7426ccc6051ae6d23
Wicker25 <wicker25@gmail.com>
2020-02-16 00:16:50 +0000

    Added Firefox 69-73

--- testing/test-cases.js
+++ testing/test-cases.js
@@ -1,7 +1,12 @@
 // prettier-ignore
 module.exports = [
+  { id: 'Firefox_69_Windows_10',   expected: { browser: 'Firefox', browserVersion: 69, layout: 'Gecko',    layoutVersion: undefined, os: 'Windows', osVersion: undefined } },
+  { id: 'Firefox_70_Windows_10',   expected: { browser: 'Firefox', browserVersion: 70, layout: 'Gecko',    layoutVersion: undefined, os: 'Windows', osVersion: undefined } },
+  { id: 'Firefox_71_Windows_10',   expected: { browser: 'Firefox', browserVersion: 71, layout: 'Gecko',    layoutVersion: undefined, os: 'Windows', osVersion: undefined } },
+  { id: 'Firefox_72_Windows_10',   expected: { browser: 'Firefox', browserVersion: 72, layout: 'Gecko',    layoutVersion: undefined, os: 'Windows', osVersion: undefined } },
   { id: 'Firefox_72_OSX_Catalina', expected: { browser: 'Firefox', browserVersion: 72, layout: 'Gecko',    layoutVersion: undefined, os: 'OS X',    osVersion: undefined } },
   { id: 'Firefox_72_Linux_x86_64', expected: { browser: 'Firefox', browserVersion: 72, layout: 'Gecko',    layoutVersion: undefined, os: 'Linux',   osVersion: undefined } },
+  { id: 'Firefox_73_Windows_10',   expected: { browser: 'Firefox', browserVersion: 73, layout: 'Gecko',    layoutVersion: undefined, os: 'Windows', osVersion: undefined } },
   { id: 'Firefox_73_Linux_x86_64', expected: { browser: 'Firefox', browserVersion: 73, layout: 'Gecko',    layoutVersion: undefined, os: 'Linux',   osVersion: undefined } },
   { id: 'Chrome_78_Linux_x86_64',  expected: { browser: 'Chrome',  browserVersion: 78, layout: 'Blink',    layoutVersion: undefined, os: 'Linux',   osVersion: undefined } },
   { id: 'Chrome_79_Windows_10',    expected: { browser: 'Chrome',  browserVersion: 79, layout: 'Blink',    layoutVersion: undefined, os: 'Windows', osVersion: undefined } },
