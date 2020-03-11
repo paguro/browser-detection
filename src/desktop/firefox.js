dadd963c6a37dfa562484888a4492c2732c179b1
Giacomo Trudu <wicker25@gmail.com>
2020-03-11 11:27:56 +0000

    Added Firefox Mobile (#42)
    * Added Firefox Mobile
    
    * Removed oldest versions
    
    * Reordered test cases
    
    * Fixed last rebase
    
    * Added more test cases
    
    * Fixed `test-cases.js`

--- src/desktop/firefox.js
+++ src/desktop/firefox.js
@@ -38,7 +38,7 @@ export function detectFirefox() {
     browserVersion = 67;
   } else if ($.hasFeature('HTMLSlotElement.prototype.assignedElements')) {
     browserVersion = 66;
-  } else if ($.hasFeature('Document.prototype.hasStorageAccess')) {
+  } else if ($.hasFeature('HTMLMarqueeElement')) {
     browserVersion = 65;
   } else if ($.hasFeature('Document.prototype.exitFullscreen')) {
     browserVersion = 64;
@@ -88,106 +88,8 @@ export function detectFirefox() {
     browserVersion = 43;
   } else if ($.hasFeature('Reflect')) {
     browserVersion = 42;
-  } else if ($.hasFeature('Document.prototype.fonts')) {
-    browserVersion = 41;
-  } else if ($.hasFeature('AudioBufferSourceNode.prototype.detune')) {
-    browserVersion = 40;
-  } else if ($.hasFeature('Cache')) {
-    browserVersion = 39;
-  } else if ($.hasFeature('BroadcastChannel')) {
-    browserVersion = 38;
-  } else if ($.hasFeature('BaseAudioContext.prototype.createStereoPanner')) {
-    browserVersion = 37;
-  } else if (
-    $.hasFeature('CanvasRenderingContext2D.prototype.resetTransform')
-  ) {
-    browserVersion = 36;
-  } else if ($.hasFeature('Element.prototype.closest')) {
-    browserVersion = 35;
-  } else if ($.hasFeature('Crypto.prototype.subtle')) {
-    browserVersion = 34;
-  } else if ($.hasFeature('CSSCounterStyleRule')) {
-    browserVersion = 33;
-  } else if (
-    $.hasFeature('CanvasRenderingContext2D.prototype.drawFocusIfNeeded')
-  ) {
-    browserVersion = 32;
-  } else if ($.hasFeature('CSS.escape')) {
-    browserVersion = 31;
-  } else if ($.hasFeature('HTMLLinkElement.prototype.relList')) {
-    browserVersion = 30;
-  } else if ($.hasFeature('Gamepad')) {
-    browserVersion = 29;
-  } else if (
-    $.hasFeature('CanvasRenderingContext2D.prototype.drawFocusIfNeeded')
-  ) {
-    browserVersion = 28;
-  } else if (
-    $.hasFeature('CanvasRenderingContext2D.prototype.lineDashOffset')
-  ) {
-    browserVersion = 27;
-  } else if ($.hasFeature('Crypto')) {
-    browserVersion = 26;
-  } else if ($.hasFeature('AnalyserNode')) {
-    browserVersion = 25;
-  } else if ($.hasFeature('Comment')) {
-    browserVersion = 24;
-  } else if ($.hasFeature('AnimationEvent')) {
-    browserVersion = 23;
-  } else if ($.hasFeature('BlobEvent')) {
-    browserVersion = 22;
-  } else if ($.hasFeature('Element.prototype.scrollHeight')) {
-    browserVersion = 21;
-  } else if ($.hasFeature('CSSConditionRule')) {
-    browserVersion = 20;
-  } else if ($.hasFeature('CSSPageRule')) {
-    browserVersion = 19;
-  } else if ($.hasFeature('Document.prototype.hidden')) {
-    browserVersion = 18;
-  } else if ($.hasFeature('CSSMediaRule')) {
-    browserVersion = 17;
-  } else if ($.hasFeature('BatteryManager')) {
-    browserVersion = 16;
-  } else if ($.hasFeature('File.prototype.lastModified')) {
-    browserVersion = 15;
-  } else if ($.hasFeature('HTMLImageElement.prototype.x')) {
-    browserVersion = 14;
-  } else if ($.hasFeature('Blob')) {
-    browserVersion = 13;
-  } else if ($.hasFeature('DOMError')) {
-    browserVersion = 12;
-  } else if ($.hasFeature('CustomEvent')) {
-    browserVersion = 11;
-  } else if ($.hasFeature('Document.prototype.fullscreenEnabled')) {
-    browserVersion = 10;
-  } else if ($.hasFeature('CompositionEvent')) {
-    browserVersion = 9;
-  } else if ($.hasFeature('CloseEvent')) {
-    browserVersion = 8;
-  } else if (
-    $.hasFeature('CanvasRenderingContext2D.prototype.lineDashOffset')
-  ) {
-    browserVersion = 7;
-  } else if ($.hasFeature('AnimationEvent')) {
-    browserVersion = 6;
-  } else if ($.hasFeature('CSSKeyframesRule.prototype.appendRule')) {
-    browserVersion = 5;
-  } else if ($.hasFeature('Blob')) {
-    browserVersion = 4;
-  } else if ($.hasFeature('CanvasGradient')) {
-    browserVersion = 3.6;
-  } else if ($.hasFeature('CanvasRenderingContext2D.prototype.font')) {
-    browserVersion = 3.5;
-  } else if ($.hasFeature('CanvasRenderingContext2D.prototype.setTransform')) {
-    browserVersion = 3;
-  } else if (
-    $.hasFeature('CanvasRenderingContext2D.prototype.createImageData')
-  ) {
-    browserVersion = 2;
-  } else if ($.hasFeature('BeforeUnloadEvent')) {
-    browserVersion = 1.5;
-  } else if ($.hasFeature('Attr')) {
-    browserVersion = 1;
+  } else {
+    return;
   }
 
   return {
