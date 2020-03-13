4c5387c201e67500ee1d355ad9e0d9624c1d3a75
Wicker25 <wicker25@gmail.com>
2020-03-13 11:04:14 +0000

    Improvement of the Chromium-based detections

--- src/desktop/brave.js
+++ src/desktop/brave.js
@@ -1,6 +1,18 @@
 import $ from '../helpers';
 import { detectChromium } from './chromium';
 
+var BRAVE_VERSION_MAP = {
+  72: 0.6,
+  73: 0.61,
+  74: 0.63,
+  75: 0.65,
+  76: 0.67, // or 0.68 if Brave supports the plugins
+  77: 0.69,
+  78: 1.0,
+  79: 1.1,
+  80: 1.3
+};
+
 export function detectBrave() {
   var plugins = $.getFeature('navigator.plugins');
 
@@ -9,45 +21,23 @@ export function detectBrave() {
 
   // Brave is based on Chromium
   var chromiumFeatures = detectChromium();
+
   if (!chromiumFeatures) {
     return;
   }
 
-  // Brave does not support the Native Client plugin (internal-nacl-plugin)
-  if ($.hasFeature('navigator.plugins.Native Client')) {
+  // Plugins have been introduced in v0.68
+  // The number of supported plugins is just 2 despite other Chromium-based browsers
+  if (plugins.length !== 0 && plugins.length !== 2) {
     return;
   }
 
-  var chromiumVersion = chromiumFeatures.browserVersion;
+  browserVersion = BRAVE_VERSION_MAP[chromiumFeatures.browserVersion];
 
-  // Plugins have been introduced in v0.68
-  // The number of supported plugins is just 2 and despite other Chromium-based browsers
-  if (plugins.length === 2) {
-    if (chromiumVersion === 80) {
-      browserVersion = 1.3;
-    } else if (chromiumVersion === 79) {
-      browserVersion = 1.1;
-    } else if (chromiumVersion === 78) {
-      browserVersion = 1.0;
-    } else if (chromiumVersion === 77) {
-      browserVersion = 0.69;
-    } else if (chromiumVersion === 76) {
-      browserVersion = 0.68;
-    }
-  } else if (plugins.length === 0) {
-    if (chromiumVersion === 76) {
-      browserVersion = 0.67;
-    } else if (chromiumVersion === 75) {
-      browserVersion = 0.65;
-    } else if (chromiumVersion === 74) {
-      browserVersion = 0.63;
-    } else if (chromiumVersion === 73) {
-      browserVersion = 0.61;
-    } else if (chromiumVersion === 72) {
-      browserVersion = 0.6;
-    }
-  } else {
-    return;
+  // Both 0.67 and 0.68 are based on the same Chromium version but the 0.68
+  // supports plugins
+  if (browserVersion === 0.67 && plugins.length === 2) {
+    browserVersion = 0.68;
   }
 
   return Object.assign(chromiumFeatures, {
